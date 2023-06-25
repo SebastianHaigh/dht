62fcceebcdc8b94d95856d8797d9cae4c97b24ce
Sebastian Haigh <sebhaigh27@googlemail.com>
2023-06-25 13:50:27 +0000

    test algos: Add node config on test node construction
    The NodeConfig contains the id of the node being created and the IP
    addresses (uint32_t) of the neighbours of that node.
    
    The maximum number of neighbours is 5, but less can be used. The first
    xero in the neighbours array represents the end of the list of
    neighbouring nodes.

--- src/rpcs/RPCs.cpp
+++ src/rpcs/RPCs.cpp
@@ -1,5 +1,16 @@
 #include "RPCs.h"
 
+Node::Node(const SimulatedNode& node, NodeConfig config)
+  : m_node(node),
+  m_id(config.m_id)
+{
+  for (unsigned int neighbourIpAddress : config.m_neighbourIpAddresses)
+  {
+    if (neighbourIpAddress == 0) return;
+
+    m_neighbours.push_back(neighbourIpAddress);
+  }
+};
 
 void Node::start(const OpType& opType)
 {

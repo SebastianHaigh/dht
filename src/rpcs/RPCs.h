8c98c551a18cfa7592c1f793e7173ec1f6bec8da
Sebastian Haigh <sebhaigh27@googlemail.com>
2023-06-27 09:01:06 +0000

    test algos: Change neighbour lists stored in test nodes
    The test node were only storing the IP addresses of the neighbours. This
    has been changed to store both the IP addressess and the node IDs in
    separate lists.
    
    This change should help in developing the algorithm for learning the
    comms graph.

--- src/rpcs/RPCs.h
+++ src/rpcs/RPCs.h
@@ -6,10 +6,44 @@ enum class OpType
   BUILD_SPANNING_TREE,
 };
 
+enum class MessageType : uint16_t
+{
+  POSITION,
+};
+
 struct NodeConfig
 {
   int m_id;
   uint32_t m_neighbourIpAddresses[5];
+};
+
+struct PositionMessage
+{
+  PositionMessage() : id(0), m_neighbours{0} {};
+
+  PositionMessage(int id, uint32_t* neighbours, size_t numNeighbours)
+    : id(id),
+      m_neighbours{0}
+  {
+    numNeighbours = (numNeighbours > 5) ? 5 : numNeighbours;
+
+    for (int i = 0; i < numNeighbours; i++)
+    {
+      m_neighbours[i] = neighbours[i];
+    }
+  }
+
+  PositionMessage(int id, std::vector<uint32_t> neighbours)
+    : id(id),
+      m_neighbours{0}
+  {
+    for (int i = 0; i < 5; i++)
+    {
+      m_neighbours[i] = neighbours[i];
+    }
+  }
+  int id;
+  uint32_t m_neighbours[5];
 };
 
 class Node
@@ -27,7 +61,8 @@ class Node
     const SimulatedNode& m_node;
 
     int m_id;
-    std::vector<uint32_t> m_neighbours;
+    std::vector<uint32_t> m_neighbourIps;
+    std::vector<int> m_neighbourIds;
 
     std::vector<int> m_nodesKnown;
     std::vector<std::pair<int, int>> m_channelsKnown;

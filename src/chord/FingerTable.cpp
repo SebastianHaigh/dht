ea7bcbe40ea4a4ba5f59f978f67e9713e52c1406
Sebastian Haigh <sebhaigh27@googlemail.com>
2023-07-31 16:23:49 +0000

    chord: add next field to finger table
    Also, initialise node IDs for every entry in the finger table.

--- src/chord/FingerTable.cpp
+++ src/chord/FingerTable.cpp
@@ -4,14 +4,18 @@ namespace chord {
 
 void initialiseFingerTable(FingerTable& fingerTable, const NodeId& nodeId)
 {
+  fingerTable.m_next = 0;
+
   // The first interval starts at nodeId
   fingerTable.m_fingers[0].m_start = nodeId;
   fingerTable.m_fingers[0].m_end = nodeId + NodeId::powerOfTwo(0);
+  fingerTable.m_fingers[0].m_nodeId = nodeId;
 
   for (int i = 1; i < fingerTable.m_fingers.size(); i++)
   {
     fingerTable.m_fingers[i].m_start = fingerTable.m_fingers[i - 1].m_end + NodeId::powerOfTwo(0);
     fingerTable.m_fingers[i].m_end = nodeId + NodeId::powerOfTwo(i);
+    fingerTable.m_fingers[i].m_nodeId = nodeId;
   }
 }
 
